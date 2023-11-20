7e91606e4fae6101bc7aa3ef9069e026d61b6f69
Mudit5149 <86524837+Mudit5149@users.noreply.github.com>
2023-11-20 17:36:20 +0530

    Add files via upload

---
+++ Core_Java_AssignmentQ5.java
@@ -0,0 +1,35 @@
+package simple_logic;
+
+import java.util.Scanner;
+
+public class Core_Java_AssignmentQ5 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter your CTC");
+		double ctc = input.nextDouble();
+		double taxAmount;
+		
+		if((ctc>0)&&(ctc>180000)) {
+			System.out.println("You belong to SLAB-A And the tax payable is nil");
+			
+			
+		}
+		if((ctc>180000)&&(ctc>300000)) {
+			taxAmount=(ctc*0.10);
+			System.out.println("You belong to SLAB-B and tax payable is :"+taxAmount);
+		}
+		else if((ctc>300000)&&(ctc>500000)) {
+			taxAmount=(ctc*0.20);
+			System.out.println("You belong to SLAB-c and tax payable is :"+taxAmount);
+		}
+		if((ctc>500000)&&(ctc>1000000)) {
+			taxAmount=(ctc*0.30);
+			System.out.println("You belong to SLAB-D and tax payable is :"+taxAmount);
+		}
+
+
+
+	}
+
+}
